24a1ea29975260b4cceef005a2aefb08aa2fd431
lindltaylor7 <riuyagami@gmail.com>
2024-09-25 19:00:36 -0500

    upd kotlin

--- app/src/main/java/com/example/todoapp/ToDoListPage.kt
+++ app/src/main/java/com/example/todoapp/ToDoListPage.kt
@@ -1,14 +1,27 @@
 package com.example.todoapp
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 @Composable
 fun ToDoListPage(){
@@ -22,9 +35,43 @@ fun ToDoListPage(){
         LazyColumn (
             content = {
                 itemsIndexed(todoList){index:Int, item: Todo ->
-                    Text(text = item.toString())
+                    TodoItem(item = item)
                 }
             }
         )
+    }
+}
+
+@Composable
+fun TodoItem(item: Todo){
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(16.dp)
+    ){
+        Column (
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = SimpleDateFormat("HH:mm:aa dd/mm", Locale.ENGLISH).format(item.createdAt),
+                fontSize = 12.sp,
+                color = Color.LightGray
+            )
+            Text(
+                text = item.title,
+                fontSize = 20.sp,
+                color = Color.White
+            )
+            IconButton(onClick = {  }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_delete_24),
+                    contentDescription = "Delete",
+                    tint = Color.White
+                )
+            }
+        }
     }
 }
